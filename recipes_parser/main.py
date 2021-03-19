75a3e0c4df20baf9a85344ff7087fde3a58868ae
igorbasko01 <igorbasko@gmail.com>
2021-03-19 23:44:55 +0200

    use vars for settings

--- recipes_parser/main.py
+++ recipes_parser/main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     logging.info(f"Reading the following settings file {settings_path}")
     config_parer = ConfigReader(settings_path)
     youtube_downloader = YoutubeDownloader(
-        config_parer.settings['google-api-key'],
-        config_parer.settings['description-output-path']
+        config_parer.google_api_key,
+        config_parer.channel_output_path
     )
-    youtube_downloader.fetch_videos_of_channel(config_parer.settings['youtube-channel'])
+    youtube_downloader.fetch_videos_of_channel(config_parer.youtube_channel)
--- recipes_parser/utils/config_reader.py
+++ recipes_parser/utils/config_reader.py
@@ -5,3 +5,6 @@ class ConfigReader(object):
     def __init__(self, config_path):
         with open(config_path, 'r') as f:
             self.settings = json.load(f)
+            self.google_api_key = self.settings['google-api-key']
+            self.youtube_channel = self.settings['youtube-channel']
+            self.channel_output_path = self.settings['channel-output-path']
