0408c6fc2024de800fbdcb52d60aca3d9ce1a0f9
igorbasko01 <igorbasko@gmail.com>
2021-03-25 22:51:48 +0200

    add ingredients section extraction logic

--- recipes_parser/main.py
+++ recipes_parser/main.py
@@ -1,6 +1,7 @@
 import logging
 from recipes_parser.utils.config_reader import ConfigReader
 from recipes_parser.downloader.youtube_description_downloader import YoutubeDownloader
+from recipes_parser.parsers.ingredients_parser import IngredientsParser
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
@@ -8,11 +9,18 @@ if __name__ == '__main__':
     settings_path = '../settings.json'
 
     logging.info(f"Reading the following settings file {settings_path}")
-    config_parer = ConfigReader(settings_path)
+    config_parser = ConfigReader(settings_path)
+
     youtube_downloader = YoutubeDownloader(
-        config_parer.google_api_key,
-        config_parer.channel_output_path,
-        config_parer.videos_output_path
+        config_parser.google_api_key,
+        config_parser.channel_output_path,
+        config_parser.videos_output_path
     )
-    youtube_downloader.prepare_channel_results(config_parer.youtube_channel)
+    youtube_downloader.prepare_channel_results(config_parser.youtube_channel)
     youtube_downloader.prepare_video_results()
+
+    ingredients_parser = IngredientsParser(
+        config_parser.videos_output_path,
+        config_parser.ingredients_section_output_path
+    )
+    ingredients_parser.run()
--- recipes_parser/parsers/ingredients_parser.py
+++ recipes_parser/parsers/ingredients_parser.py
@@ -1,3 +1,51 @@
+import recipes_parser.utils.file_ops as fops
+import itertools
+import re
+import logging
+
+
 class IngredientsParser(object):
-    def read_videos_file(self):
-        raise NotImplementedError
+    def __init__(self, videos_path, ingredients_section_path, store_results=fops.save_json_to_file):
+        self.videos_path = videos_path
+        self.ingredients_sections_path = ingredients_section_path
+        self.store_results = store_results
+
+    def run(self):
+        videos = self.read_videos()
+        videos_with_ingredients = [self.extract_ingredients_section(video) for video in videos]
+        self.store_results(videos_with_ingredients, self.ingredients_sections_path)
+
+    def read_videos(self):
+        paths = fops.get_files_of_path(self.videos_path)
+        extracted_videos = [self.extract_simplified_videos(fops.read_json_file(path)) for path in paths]
+        flattened = list(itertools.chain(*extracted_videos))
+        return flattened
+
+    def extract_ingredients_section(self, video):
+        logging.info(f"Parsing description of: {video['id']}")
+        return {
+            'id': video['id'],
+            'title': video['title'],
+            'ingredients': self.extract_ingredients_section_from_text(video['description'])
+        }
+
+    def extract_simplified_videos(self, videos):
+        return [
+            {
+                'id': video['id'],
+                'title': video['snippet']['title'],
+                'description': video['snippet']['description']
+            } for video in videos['items']
+            if 'id' in video
+        ]
+
+    def extract_ingredients_section_from_text(self, text):
+        prefix = 'ingredients:'
+        suffix = 'directions:'
+        p = re.compile(f'{prefix}.*{suffix}', re.IGNORECASE | re.MULTILINE | re.DOTALL)
+        regex_result = p.search(text)
+        result = None
+        if regex_result:
+            extracted_string = regex_result.group()
+            result = extracted_string[len(prefix):len(extracted_string)-len(suffix)]
+        return result
