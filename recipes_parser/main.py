ca8a48ae79e24ca1dbc2d49c8b69b34f7e5c49f2
igorbasko01 <igorbasko@gmail.com>
2021-03-27 16:58:51 +0300

    predict ingredients

--- recipes_parser/main.py
+++ recipes_parser/main.py
@@ -11,16 +11,19 @@ if __name__ == '__main__':
     logging.info(f"Reading the following settings file {settings_path}")
     config_parser = ConfigReader(settings_path)
 
-    youtube_downloader = YoutubeDownloader(
-        config_parser.google_api_key,
-        config_parser.channel_output_path,
-        config_parser.videos_output_path
-    )
-    youtube_downloader.prepare_channel_results(config_parser.youtube_channel)
-    youtube_downloader.prepare_video_results()
+    # youtube_downloader = YoutubeDownloader(
+    #     config_parser.google_api_key,
+    #     config_parser.channel_output_path,
+    #     config_parser.videos_output_path
+    # )
+    # youtube_downloader.prepare_channel_results(config_parser.youtube_channel)
+    # youtube_downloader.prepare_video_results()
 
     ingredients_parser = IngredientsParser(
         config_parser.videos_output_path,
-        config_parser.ingredients_section_output_path
+        config_parser.ingredients_section_output_path,
+        config_parser.ingredient_parser_container_name,
+        config_parser.crf_model_path_in_container,
+        config_parser.predicted_ingredients_output_path
     )
     ingredients_parser.run()
--- recipes_parser/parsers/ingredients_parser.py
+++ recipes_parser/parsers/ingredients_parser.py
@@ -2,18 +2,31 @@ import recipes_parser.utils.file_ops as fops
 import itertools
 import re
 import logging
+import subprocess
+import json
 
 
 class IngredientsParser(object):
-    def __init__(self, videos_path, ingredients_section_path, store_results=fops.save_json_to_file):
+    def __init__(self,
+                 videos_path,
+                 ingredients_section_path,
+                 container_name,
+                 crf_model_path,
+                 predicted_ingredients_path,
+                 store_results=fops.save_json_to_file):
         self.videos_path = videos_path
         self.ingredients_sections_path = ingredients_section_path
         self.store_results = store_results
+        self.container_name = container_name
+        self.crf_model_path = crf_model_path
+        self.predicted_ingredients_path = predicted_ingredients_path
 
     def run(self):
         videos = self.read_videos()
         videos_with_ingredients = [self.extract_ingredients_section(video) for video in videos]
         self.store_results(videos_with_ingredients, self.ingredients_sections_path)
+        predicted = [self.predict_ingredients(video) for video in videos_with_ingredients if video['ingredients']]
+        self.store_results(predicted, self.predicted_ingredients_path)
 
     def read_videos(self):
         paths = fops.get_files_of_path(self.videos_path)
@@ -49,3 +62,22 @@ class IngredientsParser(object):
             extracted_string = regex_result.group()
             result = extracted_string[len(prefix):len(extracted_string)-len(suffix)]
         return result
+
+    def predict_ingredients(self, video):
+        logging.info(f"Predicting ingredients of video: {video}")
+        echo_command = f'echo "{video["ingredients"]}" | bin/parse-ingredients.py --model-file {self.crf_model_path}'
+
+        predicted_video = {
+            'id': video['id'],
+            'title': video['title'],
+            'ingredients': video['ingredients']
+        }
+
+        try:
+            output = subprocess.check_output(
+                ['docker', 'exec', self.container_name, '/bin/bash', '-c', echo_command]).decode('utf8')
+            predicted_video['predicted_ingredients'] = json.loads(output)
+        except subprocess.CalledProcessError:
+            predicted_video['predicted_ingredients'] = None
+
+        return predicted_video
